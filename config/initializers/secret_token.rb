8cbd4129365bc23d178c672a5461ba7163a1bf21
unsnu <unsnu.kr@gmail.com>
2013-07-25 15:29:14 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unsnu::Application.config.secret_key_base = '8ba6db95a728b52a0c883cb136c772ff7a7fc065e98e0954ca81c194a425b9938399733b64329aac1d45ca78a281b548dffb08401cad4a212b6eb842e818e5ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unsnu::Application.config.session_store :cookie_store, key: '_unsnu_session'
