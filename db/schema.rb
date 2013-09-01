e87a3b9de4bededdd76cf04e2eed28b379058eb8
unsnu <unsnu.kr@gmail.com>
2013-09-01 14:46:10 +0900

    Added admin role to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130831104325) do
+ActiveRecord::Schema.define(version: 20130901054433) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20130831104325) do
     t.string   "nickname"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",       default: "", null: false
+    t.string   "encrypted_password",       default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20130831104325) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "admin",                    default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
